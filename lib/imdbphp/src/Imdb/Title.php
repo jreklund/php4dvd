98e28b3f1642a494aabbc77cff0ae276d19e1db8
Johan Eklund <hello@johaneklund.com>
2018-05-04 20:59:38 +0200

    imdbphp 6.0.1 - Patch 1

--- lib/imdbphp/src/Imdb/Title.php
+++ lib/imdbphp/src/Imdb/Title.php
@@ -805,7 +805,7 @@ class Title extends MdbBase {
   public function plotoutline($fallback=FALSE) {
     if ($this->main_plotoutline == "") {
       $page = $this->getPage("Title");
-      if (preg_match('!itemprop="description">\s*(.*?)\s*</div>!ims', $page, $match)) {
+      if (preg_match('!class="summary_text">\s*(.*?)\s*</div>!ims', $page, $match)) {
         $this->main_plotoutline = trim($match[1]);
       } elseif($fallback) {
         $this->main_plotoutline = $this->storyline();
@@ -822,17 +822,9 @@ class Title extends MdbBase {
    */
   public function storyline() {
     if ($this->main_storyline == "") {
-      $this->getPage("Title");
-      if (@preg_match('!Storyline</h2>\s*\n*<div.*?>\s*\n*<?p?>?(.*?)<?/?p?<h4!ims',$this->page["Title"],$match)) {
-        if (preg_match('!(.*?)<em class="nobr">Written by!ims',$match[1],$det))
-          $this->main_storyline = trim($det[1]);
-        elseif (preg_match('!(.*)\s</p>!ims',$match[1],$det))
-          $this->main_storyline = trim($det[1]);
-        elseif (preg_match('!(.*)\s<span class="see-more inline">!ims',$match[1],$det))
-          $this->main_storyline = trim($det[1]);
-        elseif (preg_match('!(.*)\s\|!ims',$match[1],$det))
-          $this->main_storyline = trim($det[1]);
-        else $this->main_storyline = trim($match[1]);
+      $page = $this->getPage("Title");
+      if (@preg_match('!Storyline</h2>.*?itemprop="description">(.*?)</span>!ims', $page, $match)) {
+        $this->main_storyline = trim($match[1]);
       }
     }
     return $this->main_storyline;
