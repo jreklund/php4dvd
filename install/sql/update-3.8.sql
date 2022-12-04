db7973006bb8860a1530581ec8544ca5ff8e8285
Johan Eklund <hello@johaneklund.com>
2022-12-04 14:49:20 +0100

    Replace deprecated date/datetime values

--- install/sql/update-3.8.sql
+++ install/sql/update-3.8.sql
@@ -1,2 +1,4 @@
 ALTER TABLE `movies` CHANGE `loandate` `loandate` DATE NULL DEFAULT NULL;
 ALTER TABLE `users` CHANGE `lastlogin` `lastlogin` TIMESTAMP NULL DEFAULT NULL;
+UPDATE `movies` SET `loandate` = NULL WHERE `loandate` = '0000-00-00';
+UPDATE `users` SET `lastlogin` = NULL WHERE `lastlogin` = '0000-00-00 00:00:00';
